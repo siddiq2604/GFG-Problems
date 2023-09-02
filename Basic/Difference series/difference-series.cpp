659027f7dfc6b397fdbd19ee97632add9924f906
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-09-02 22:49:44 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Difference series/difference-series.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int differenceSeries(int N)
+    {
+        // Write Your Code here
+        return (N*2+1)*N;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int ans = ob.differenceSeries(N);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
