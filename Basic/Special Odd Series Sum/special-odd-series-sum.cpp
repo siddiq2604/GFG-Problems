d72e13f74d02e001cf58c518cdd370a1da0656d1
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-09-03 17:28:40 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Special Odd Series Sum/special-odd-series-sum.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int sumOfTheSeries(long long int n){
+        // code here
+        return n*(n+1)*(2*n+1)/6;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.sumOfTheSeries(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
