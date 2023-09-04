df60cdef1395fb706996183fee035502e1c57285
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-09-04 18:55:11 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print the pattern | Set-1/print-the-pattern-set1.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+void printPat(int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n;
+	cin>>n;
+    printPat(n);
+    cout<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+void printPat(int n)
+{
+//Your code here
+for(int i=0;i<n;i++)
+    {
+       for(int j=0;j<n;j++)
+       {
+           for(int k=0;k<n-i;k++)
+           {
+               cout<<n-j<<" ";
+           }
+       }
+       cout<<"$";
+    } 
+}
