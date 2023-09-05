6bd2f7940465413307ac7e32982ce53f29ef08c6
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-09-05 07:17:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sequence Fun/sequence-fun.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+	public:
+		int NthTerm(int n){
+		    // Code  here
+		    if(n==1)
+		    return 2;
+		    long N=2;
+		    for(int i=2;i<=n;i++){
+		        N=(N*i+1)%1000000007;
+		    }
+		    return N;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution obj;
+		int ans = obj.NthTerm(n);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
