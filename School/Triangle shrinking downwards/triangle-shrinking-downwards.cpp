596586dc404635d8f968f0074838774a697512ac
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-09-08 07:21:40 +0530

    Added Solution - GfG to GitHub

---
+++ School/Triangle shrinking downwards/triangle-shrinking-downwards.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string triDownwards(string S) {
+        // code here
+        string s="";
+        
+        for(int i=0;i<S.size();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                s+=".";
+            }
+            for(int j=i;j<S.size();j++)
+            {
+                s+=S[j];
+            }
+        }
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        
+        cin >> S;
+
+        Solution ob;
+        string ans=ob.triDownwards(S);
+        
+        for(int i=0 ; i<ans.length() ; i++)
+        {
+            cout<<ans[i];
+            if((i+1)%S.length()==0)
+                cout<<endl;
+        }
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
