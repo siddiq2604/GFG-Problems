e00d8733f1879f80a4871148fa4e85cdb54afd43
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-10-03 22:09:26 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Segregate Even and Odd numbers/segregate-even-and-odd-numbers.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().segregateEvenOdd(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    void segregateEvenOdd(int arr[], int n) {
+        // code here
+        int j=0;
+        Arrays.sort(arr);
+        ArrayList<Integer> evenList = new ArrayList<>();
+        ArrayList<Integer> oddList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                evenList.add(arr[i]);
+            } else {
+                oddList.add(arr[i]);
+            }
+        }
+        for(Integer x:evenList){
+            arr[j]=x;
+            j++;
+        }
+        for(Integer x:oddList){
+            arr[j]=x;
+            j++;
+        }
+    }
+}
