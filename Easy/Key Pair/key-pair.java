af8447612cf48193fcecdff7a00f2088771ec689
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-11-10 22:13:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Key Pair/key-pair.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().hasArrayTwoCandidates(arr, n, x);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        // code here
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int t = x - arr[i];
+            if (hm.containsKey(t)) {
+                return true;
+            }
+            hm.put(arr[i], i);
+        }
+        return false;
+    }
+}
