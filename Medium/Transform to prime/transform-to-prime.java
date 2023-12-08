7b3c7e83b9c1019bd77bf24a3442bdcbb0ddd11d
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-12-08 22:00:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Transform to prime/transform-to-prime.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcases = Integer.parseInt(br.readLine());
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(a1[i]);
+            }
+            Solution ob = new Solution();
+            int ans=ob.minNumber(a,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int minNumber(int arr[], int N)
+    {
+        int result=0;
+       int data=0;
+       for(int i=0;i<N;i++)
+        data+=arr[i];
+        if(data==1) return 1;
+       
+       for(int i=2;i<=(data+result)/2;i++){
+         if((data+result)%i==0){
+             result++;
+             i=2;
+          }
+        }
+        return result;
+    }
+}
