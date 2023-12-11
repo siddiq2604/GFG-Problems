a4c855bb91f8f7b10a0f98c7274eb1cb81ebdd7e
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2023-12-11 19:41:52 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
+  
+            // Appending new elements at 
+            // the end of the list 
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                Arr.add(x); 
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        // code here
+        long ans = 0;
+        for(int i=0;i<K; i++){
+            ans += Arr.get(i);
+        }
+        long temp = ans;
+        for(int i=K; i<N; i++){
+            
+            temp -= Arr.get(i-K);
+            temp += Arr.get(i);
+            ans = Math.max(ans,temp);
+        }
+        return ans;
+    }
+}
