6f7fdf6bf597308fd72c89966f9cf4bbe420dcc6
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2024-02-06 21:23:06 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count the nodes at distance K from leaf/count-the-nodes-at-distance-k-from-leaf.cpp
@@ -0,0 +1,154 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//Node Structure
+/*struct NodeGiven a binary tree with n nodes and a non-negative integer k, the task is to count the number of special nodes. A node is considered special if there exists at least one leaf in its subtree such that the distance between the node and leaf is exactly k.
+{
+	int data;
+	Node *left, *right;
+};*/
+
+class Solution
+{
+    public:
+    void help( Node * root, int k, int level, vector<pair<int,Node *>> s, set<pair<int,Node*>>&ans){
+        if(root == NULL)return;
+        
+        if(root-> left == NULL && root->right == NULL){
+            if(k == 0){
+                pair<int,Node*> temp = make_pair(root->data, root);
+                ans.insert(temp);
+            }
+            else if(level >= k){
+                pair<int,Node*> t = s[level - k];
+                pair<int,Node*> temp = make_pair(t.first,t.second);
+                ans.insert(temp);
+            }
+        }
+        
+        s.push_back({root->data,root});
+        help(root->left, k, level+1, s, ans);
+        help(root->right, k, level+1, s, ans);
+    }
+    int printKDistantfromLeaf(Node* root, int k)
+    {
+        set<pair<int,Node*>> ans;
+        vector<pair<int,Node *>> s;
+        help(root, k, 0, s, ans);
+        return ans.size();
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s, ks;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, ks);
+        int k=stoi(ks);
+        Solution obj;
+        cout<<obj.printKDistantfromLeaf(root, k)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
