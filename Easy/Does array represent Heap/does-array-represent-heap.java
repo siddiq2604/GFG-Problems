a21f385e1cb5dbf33344a205e39dd77e9243ede5
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2024-02-17 21:40:09 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Does array represent Heap/does-array-represent-heap.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.countSub(a, n) ? 1:0);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public boolean countSub(long arr[], long n)
+    {
+        // Your code goes here
+         for(int i=0;i<arr.length;i++)
+       {
+           int left=2*i+1;
+           if(left<n)
+           { if( arr[left]>arr[i])
+               return false;
+           }
+           int right=2*i+2;
+           if(right<n)
+           { if( arr[right]>arr[i])
+               return false;
+           }  
+       }
+       return true;
+    }
+}
