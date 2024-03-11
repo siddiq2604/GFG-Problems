09cac9ea5a6d8b79956de1d416f3f64681d0dd3c
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2024-03-11 20:08:13 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reach a given score/reach-a-given-score.java
+++ Easy/Reach a given score/reach-a-given-score.java
@@ -22,19 +22,19 @@ public class GFG {
 
 class Geeks {
     public long count(int n) {
-               int[] table = new int[n + 1];
-        Arrays.fill(table, 0);
-        table[0] = 1;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
         for(int i = 3; i <= n; i++){
-            table[i] += table[i - 3];
+            dp[i] += dp[i - 3];
         }
         for(int i = 5; i <= n; i++){
-            table[i] += table[i - 5];
+            dp[i] += dp[i - 5];
         }
         for(int i = 10; i <= n; i++){
-            table[i] += table[i - 10];
+            dp[i] += dp[i - 10];
         }
-        return table[n];
+        return dp[n];
  // Add your code here.
     }
 }
