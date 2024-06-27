c160519371e7ba3cc56c03b5e249155b6954910c
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2024-06-27 15:07:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            int sum = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if(ob.isSubsetSum(N, arr, sum))
+            System.out.println(1);
+            else
+            System.out.println(0);
+
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+
+
+    static Boolean isSubsetSum(int N, int arr[], int sum){
+        // code here
+        boolean[][] dp = new boolean[N+1][sum+1];
+        
+        for(int i=0;i<=N;i++)
+            dp[i][0] = true;
+            
+        for(int j=1;j<=sum;j++)
+            dp[0][j] = false;
+            
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]<=j)
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                    
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return dp[N][sum];
+
+    }
+}
