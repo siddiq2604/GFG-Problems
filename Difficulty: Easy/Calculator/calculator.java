5c8937cd226741b3463fd8408fe72aee56bc2f31
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2025-05-29 16:15:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Calculator/calculator.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public static void utility(int a, int b, int operator) {
+
+        // write your code here
+        if(operator==1) System.out.print(a+b);
+        else if(operator==2) System.out.print(a-b);
+        else if(operator==3) System.out.print(a*b);
+        else System.out.print("Invalid Input");
+    }
+}
