84ccce0601a4cc2134cf579d48cab873f3f9fde5
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2025-05-29 16:20:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    public static String utility(int n) {
+
+        // write your code here
+        if(n%2==0){
+            return "Friend";
+        }
+        else{
+            return "You";
+        }
+    }
+}
