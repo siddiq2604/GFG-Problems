632359737ef9f1361e13651db211276c492caf39
Mohammed Abubakar Siddiq <100069065+siddiq2604@users.noreply.github.com>
2025-06-26 08:09:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decision Making in Java/decision-making-in-java.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+class Solution {
+    public static String compareNM(int n, int m) {
+        // code here
+        if(n<m) return "lesser";
+        else if(n==m) return "equal";
+        else return "greater";
+    }
+}
